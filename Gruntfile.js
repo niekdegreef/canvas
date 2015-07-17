78231f4d6fa8ed7c60427fff257b2170711e6b8b
Niek de Greef <niek@beyerdegreef.com>
2015-07-17 14:49:33 +0200

    First commit - all the files

---
+++ Gruntfile.js
@@ -0,0 +1,101 @@
+module.exports = function(grunt) {
+
+	// require('grunt-postcss')(grunt);
+
+    grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		/**
+		 * Sass
+		 */
+		sass: {
+		  dev: {
+		    options: {
+				style: 'expanded',
+				sourcemap: 'auto',
+				require: 'susy'
+		    },
+		    files: {
+		    	'style.css': 'scss/style.scss'
+		    },		    
+		  }
+		},
+
+		/*
+		* Autoprefixer (via postcss)
+		*/
+
+		postcss: {
+		    options: {
+		      map: true, // inline sourcemaps
+		      processors: [
+		        require('pixrem')(), // add fallbacks for rem units
+		        require('autoprefixer-core')({browsers: 'last 2 versions'}), // add vendor prefixes
+		        require('cssnano')() // minify the result
+		      ]
+		    },
+		    dist: {
+		      src: '*.css'
+		    }
+		  },
+
+		/**
+		* Browser sync / https://css-tricks.com/cross-browser-css-injection/
+		*/
+		browserSync: {
+		    bsFiles: {
+		        src : ["style.css", "README.html"]
+		    },
+		    options: {
+		    	watchTask: true,
+	            proxy: "http://localhost/canvas",
+	            injectChanges: true
+	        },
+		},
+
+		/**
+		* Markdown
+		*/
+
+		markdown: {
+		all: {
+		  files: [
+		    {
+		      expand: true,
+		      src: '*.md',
+		      dest: '',
+		      ext: '.html'
+		    }
+		  ]
+		}
+		},
+
+	  	/**
+	  	 * Watch
+	  	 */
+		watch: {
+			sass: {
+				files: ['scss/*.scss'] ,
+				tasks: ['sass' , 'postcss' ],
+				options: {
+					livereload: 35729,
+				},
+			},
+			markdown: {
+				files: ['*.md'] ,
+				tasks: ['markdown'],
+				options: {
+					livereload: 35729,
+				},
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-markdown');
+	grunt.registerTask('default',['browserSync','watch']);
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # canvas
-Personal starter site
+Personal starter site.
+
+While you're welcome to use anything you find here, I strongly advise you not to, this is all for a personal project, enabling me to play around with a variety of tools. 
+
+## Personal dev notes
+
+* Grunt plugins installed locally, repeat for further projects
+* Susy installed globally
+* http://www.lynda.com/Grunt.js-tutorials/Installing-Grunt-cli-Sass
+* Globally installed:
+	
+	* Grunt-cli
+	* Sass (= ruby application)
+	
+* To install Local (in folder)
+	
+	* Grunt
+
+## SETUP Steps
+
+1. cd to directory
+1. type 'npm init' to start setting up project
+1. fill in the values (name etc)
+1. package.json is now created
+1. install Sass: npm install grunt-contrib-sass --save-dev
+1. install Watch: npm install grunt-contrib-watch --save-dev
+1. install Postcss (and dependencies), this is to use Autoprefixer: npm install grunt-postcss pixrem autoprefixer-core cssnano --save-dev
+1. install Browser (https://css-tricks.com/cross-browser-css-injection/) sync: npm install grunt-browser-sync --save-dev
+1. make sure to add a proxy in options under BrowserSync for css injection
+1. add Markdown npm install grunt-markdown --save-dev
+
+###To Do
+
+* Use Libsass
